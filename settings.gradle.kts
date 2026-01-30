da56148b308b045b31626b98d6c85c87c4ac962a
JulienTAP <tap.julien91@gmail.com>
2026-01-30 09:25:57 +0100

    fix merge

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,22 @@ pluginManagement {
         google()
     }
 }
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        maven {
+            url = uri("https://packages.confluent.io/maven/")
+        }
+    }
+}
 rootProject.name = "projet-global"
 
 include("schema-lib")
